e2c3bb756a5e2197292a0e96ab0862e38da4d978
Rick Lambrechts <rick.lambrechts@hotmail.com>
2022-03-24 12:25:35 +0100

    Updated service provider

--- src/InformerOnlineServiceProvider.php
+++ src/InformerOnlineServiceProvider.php
@@ -4,6 +4,7 @@ namespace RLWebdiensten\LaravelInformeronline;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use RLWebdiensten\LaravelInformeronline\Contracts\InformerOnlineConfig as Config;
 
 class InformerOnlineServiceProvider extends PackageServiceProvider
 {
@@ -20,7 +21,7 @@ class InformerOnlineServiceProvider extends PackageServiceProvider
 
         $this->app->alias(InformerOnline::class, 'laravel-informeronline');
 
-        $this->app->singleton(InformerOnline::class, function () {
+        $this->app->singleton(Config::class, function () {
             return new InformerOnlineConfig(strval(config('informeronline.base_uri')), strval(config('informeronline.api_key')), intval(config('informeronline.security_code')));
         });
     }
