5518a4b067b15d419d1c2299d27b0e06a2426017
faab007nl <faab007nl@users.noreply.github.com>
2022-03-24 14:58:28 +0000

    Fix styling

--- src/Abstracts/PurchaseInvoiceStatus.php
+++ src/Abstracts/PurchaseInvoiceStatus.php
@@ -4,15 +4,14 @@ namespace RLWebdiensten\LaravelInformerOnline\Abstracts;
 
 class PurchaseInvoiceStatus
 {
-    const SALES_INVOICE_STATUS_OPEN = 'open';
-    const SALES_INVOICE_STATUS_EXPIRED = 'expired';
-    const SALES_INVOICE_STATUS_PROCESS = 'process';
-    const SALES_INVOICE_STATUS_PAID = 'paid';
-    const SALES_INVOICE_STATUS_APPROVE = 'approve';
+    public const SALES_INVOICE_STATUS_OPEN = 'open';
+    public const SALES_INVOICE_STATUS_EXPIRED = 'expired';
+    public const SALES_INVOICE_STATUS_PROCESS = 'process';
+    public const SALES_INVOICE_STATUS_PAID = 'paid';
+    public const SALES_INVOICE_STATUS_APPROVE = 'approve';
 
     public static function in_array($status): bool
     {
         return in_array($status, [self::SALES_INVOICE_STATUS_OPEN, self::SALES_INVOICE_STATUS_EXPIRED, self::SALES_INVOICE_STATUS_PROCESS, self::SALES_INVOICE_STATUS_PAID, self::SALES_INVOICE_STATUS_APPROVE], true);
     }
-
 }
--- src/Abstracts/SalesInvoiceStatus.php
+++ src/Abstracts/SalesInvoiceStatus.php
@@ -4,14 +4,13 @@ namespace RLWebdiensten\LaravelInformerOnline\Abstracts;
 
 class SalesInvoiceStatus
 {
-    const SALES_INVOICE_STATUS_OPEN = 'open';
-    const SALES_INVOICE_STATUS_EXPIRED = 'expired';
-    const SALES_INVOICE_STATUS_COLLECT = 'collect';
-    const SALES_INVOICE_STATUS_DRAFT = 'draft';
+    public const SALES_INVOICE_STATUS_OPEN = 'open';
+    public const SALES_INVOICE_STATUS_EXPIRED = 'expired';
+    public const SALES_INVOICE_STATUS_COLLECT = 'collect';
+    public const SALES_INVOICE_STATUS_DRAFT = 'draft';
 
     public static function in_array($status): bool
     {
         return in_array($status, [self::SALES_INVOICE_STATUS_EXPIRED, self::SALES_INVOICE_STATUS_COLLECT, self::SALES_INVOICE_STATUS_DRAFT], true);
     }
-
 }
